d7ae7c43a90a0de7599d5a5203a4853e2e2d13f2
Juan Pa Bedoya <bedoyavascojuanpablo@gmail.com>
2023-04-16 15:37:48 -0500

    📦 NEW: add output code mirror layout

--- src/components/Output/index.tsx
+++ src/components/Output/index.tsx
@@ -1,3 +1,5 @@
+import { javascript } from "@codemirror/lang-javascript"
+import CodeMirror from "@uiw/react-codemirror"
 
 export const Output = () => {
 
@@ -5,6 +7,14 @@ export const Output = () => {
   return (
     <>
       <h1>Output</h1>
+      <CodeMirror
+        theme={"dark"}
+        value="console.log('hello world!');"
+        height="200px"
+        // editable={false}
+        extensions={[javascript({ jsx: true })]}
+        onChange={(value) => console.log(value)}
+      />
     </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head"
 import { Inter } from "next/font/google"
 import { Input } from "@/components/Input"
+import { Output } from "@/components/Output"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <>
       <Input/>
+      <Output/>
     </>
   )
 }
