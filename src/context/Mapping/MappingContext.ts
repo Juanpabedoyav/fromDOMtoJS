87c0ca8f3bd339262cdb71d2b23d812d80e94d2f
Juan Pa Bedoya <bedoyavascojuanpablo@gmail.com>
2023-04-16 18:00:42 -0500

    📦 NEW: add theme to input field

--- src/context/Mapping/MappingContext.ts
+++ src/context/Mapping/MappingContext.ts
@@ -1,7 +1,8 @@
 import { createContext } from "react"
 
 export interface MappingContextProps {
-    body:string,
-    mapping: string
+    body: string,
+    mapping:string,
+    dispatch?: any
 }
 export const MappingContext = createContext({} as MappingContextProps)
--- src/context/Mapping/MappingProvider.tsx
+++ src/context/Mapping/MappingProvider.tsx
@@ -1,17 +1,29 @@
+import { useReducer } from "react"
 import { MappingContext } from "./MappingContext"
+import { MappingReducer } from "./MappingReducer"
 
 interface MappingProviderProps {
     children: JSX.Element | JSX.Element[]
 }
-const INITIAL_STATE = {
+
+export interface MappingState{
+    body: string,
+    mapping: string,
+}
+const INITIAL_STATE: MappingState = {
   body: "",
-  mapping: ""
+  mapping: "",
 }
 
 
 export const MappingProvider = ({children}: MappingProviderProps) => {
+  const [state, dispatch] = useReducer(MappingReducer, INITIAL_STATE)
   return (
-    <MappingContext.Provider value={INITIAL_STATE}>
+    <MappingContext.Provider value={{
+      ...state, 
+      dispatch
+    }
+    }>
       {children}
     </MappingContext.Provider>
   )
