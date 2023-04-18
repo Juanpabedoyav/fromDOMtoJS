84eea56553d7978b4e8ab2bfddd227b3685b2fb7
Juan Pa Bedoya <bedoyavascojuanpablo@gmail.com>
2023-04-17 19:16:11 -0500

    📦 NEW: add prompt to generate prompr

--- src/components/Generate/index.tsx
+++ src/components/Generate/index.tsx
@@ -1,11 +1,13 @@
 import { MappingContext } from "@/context/Mapping/MappingContext"
 import { SectionGenerateSC } from "@/screens/main/styles"
+import { askGpt } from "@/utils/askGpt"
 import { useContext, useEffect, useState } from "react"
 
 
 export const Generate = () => {
   const [text, setText] =  useState("Happy Scrapping!!")
   const {dispatch, body}  =useContext(MappingContext)
+  const prompt = `Act as a Senior Javascript Developre with deep knowledge in DOM manipulation, keep in mind this HTML structure ${body} and generate the code to scrap the data.`
 
   useEffect(() => {
     const time = setTimeout(() => {
@@ -19,8 +21,9 @@ export const Generate = () => {
   const handleClick =() => {
     console.log("click")
     setText("Generate Code ...")
+    const info = askGpt(prompt)
+    console.log(info)
     dispatch({type: "SET_MAPPING", payload: body})
-    // askGpt()
   }
   return (
     <SectionGenerateSC>
--- src/utils/askGpt.ts
+++ src/utils/askGpt.ts
@@ -1,17 +1,16 @@
 const { Configuration, OpenAIApi } = require("openai")
 
-export const askGpt = async () => {
+export const askGpt = async (prompt: string) => {
 
   const configuration = new Configuration({
-    apiKey: "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",
+    apiKey: "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",
   })
   const openai = new OpenAIApi(configuration)
-    
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
-    messages: [{role: "user", content: "Hello world"}],
+    messages: [{role: "user", content: prompt}],
     temperature: 0.9,
   })
-  console.log(completion.data.choices[0].message)
 
+  console.log(completion.data.choices[0].message)
 }
