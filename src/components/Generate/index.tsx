5e725dcfa1ba4d6f3b5fbdddff05accd2dbb0717
Juan Pa Bedoya V <bedoyavascojuanpablo@gmail.com>
2023-04-23 21:11:49 -0500

    📦 NEW: add first prompt for to generate JS

--- src/components/Generate/index.tsx
+++ src/components/Generate/index.tsx
@@ -7,23 +7,23 @@ import { useContext, useEffect, useState } from "react"
 export const Generate = () => {
   const [text, setText] =  useState("Happy Scrapping!!")
   const {dispatch, body}  =useContext(MappingContext)
-  const prompt = `Act as a Senior Javascript Developre with deep knowledge in DOM manipulation, keep in mind this HTML structure ${body} and generate the code to scrap the data.`
-
   useEffect(() => {
     const time = setTimeout(() => {
       setText("Happy Scrapping!!")
     }, 1500)
-
+    
     return () => clearTimeout(time)
   }, [text])
-    
-
-  const handleClick =() => {
-    console.log("click")
+  
+  
+  const handleClick =  async() => {
+    const prompt = `Act as a Javascript developer with focus in DOM find the patterns ${body.slice(0,1000)} create a Javascript function with the pattern for extract info`
     setText("Generate Code ...")
-    const info = askGpt(prompt)
-    console.log(info)
-    dispatch({type: "SET_MAPPING", payload: body})
+    askGpt(prompt).then(e =>{
+      dispatch({type: "SET_MAPPING", payload: e})
+
+    })
+  
   }
   return (
     <SectionGenerateSC>
--- src/utils/askGpt.ts
+++ src/utils/askGpt.ts
@@ -1,16 +1,16 @@
 const { Configuration, OpenAIApi } = require("openai")
 
-export const askGpt = async (prompt: string) => {
+export const askGpt = async (prompt : string) => {
 
   const configuration = new Configuration({
-    apiKey: "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",
+    apiKey: "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",
   })
   const openai = new OpenAIApi(configuration)
   const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
+    model: "gpt-3.5-turbo-0301",
     messages: [{role: "user", content: prompt}],
     temperature: 0.9,
   })
 
-  console.log(completion.data.choices[0].message)
+  return completion.data.choices[0].message.content
 }
